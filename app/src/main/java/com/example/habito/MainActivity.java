a030f9f8c1f0e32947b7ae6ed64ddaad5e8a46eb
Tomáš Lokša <tomas.loksa@outlook.com>
2020-08-11 19:38:46 +0200

    edit button

--- app/src/main/java/com/example/habito/MainActivity.java
+++ app/src/main/java/com/example/habito/MainActivity.java
@@ -22,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton add_button = findViewById(R.id.add_button);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
